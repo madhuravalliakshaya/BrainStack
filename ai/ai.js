65e09f3d621768b8c06fe590aec0c7a55be3ea0e
madhuravalliakshaya <minibajinki@gmail.com>
2025-09-30 16:51:12 +0530

    commit

---
+++ ai/ai.js
@@ -0,0 +1,36 @@
+import express from  'express'
+import path from 'path'
+import cors from 'cors'
+import bodyParser from 'body-parser';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+dotenv.config();
+const __filename=fileURLToPath(import.meta.url)
+
+const __dirname=path.dirname(__filename)
+const app=express();
+app.use(cors())
+app.use(express.json())
+
+app.use(express.static(path.join(__dirname,'public')))
+app.use(bodyParser.urlencoded({extended:true}))
+import { GoogleGenAI } from "@google/genai";
+ const API=process.env.API_KEY
+const ai = new GoogleGenAI({apiKey:API});
+var t=''
+async function main(content) {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash-lite",
+    contents: content,
+  });
+ t=response.text;
+ return t
+}
+app.post('/submit', async (req,res)=>{
+const k= await main(req.body.prompt)
+    res.send({ans:k})
+    console.log(k)
+})
+if (process.env.API_KEY)
+    console.log("YES")
+app.listen(3000)
